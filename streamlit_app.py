9921bdf725cc79ae08332bd32418ad2d84c3cc9c
jiaaww <shaziaemirafauziah@gmail.com>
2025-05-27 09:54:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ st.write(
     "shazia cantik! shazia cantik, shazia cantik [shazia cantik](shazia cantik)."
 )
 st.image("quality_restoration_20250419210355902.jpeg")
-
-
 st.title("aplikasi sederhana")
 st.header("aplikasi mengecek nilai genap/ganjil")
 angka=st.number_input("tulis sebuah angka:", value=0, step=1)
