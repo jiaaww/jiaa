5fc161724e0534bf1eb4ae06d55276ad21ed85fb
jiaaww <shaziaemirafauziah@gmail.com>
2025-05-27 08:58:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("shazia cantik")
 st.write(
-    "Let's start building! For help and inspiration, head over to [shazia cantik](shazia cantik)."
+    "shazia cantik! For help and inspiration, head over to [shazia cantik](shazia cantik)."
 )
